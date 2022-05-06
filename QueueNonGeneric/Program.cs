5bb5ce01c6feb843a5d29117e2a5c436089f3fbb
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-05-06 15:24:02 +0530

    Add project files.

---
+++ QueueNonGeneric/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueNonGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+        //-----------Queue Enqueue-------------
+
+            queue.Enqueue("php");
+            queue.Enqueue("java");
+            queue.Enqueue("html");
+            queue.Enqueue("css");
+
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+        //---------Queue Dequeue----------------
+
+            queue.Dequeue();
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
